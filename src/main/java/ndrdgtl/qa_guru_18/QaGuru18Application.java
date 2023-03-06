b76095df2dff4134ece5a3b60ed6dc3accb46841
Adel Kurysheva <Adel.Kurysheva@nedra.digital>
2023-03-07 00:44:53 +0300

    hello world message was changed

--- src/main/java/ndrdgtl/qa_guru_18/QaGuru18Application.java
+++ src/main/java/ndrdgtl/qa_guru_18/QaGuru18Application.java
@@ -7,6 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class QaGuru18Application {
 
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hello qa_guru");
     }
 }
