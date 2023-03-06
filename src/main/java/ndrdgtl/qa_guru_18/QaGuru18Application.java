b0d1c0582242b01f6a35e42198f6e571b19e696a
AdelKurysheva <127086310+AdelKurysheva@users.noreply.github.com>
2023-03-07 00:54:53 +0300

    Update QaGuru18Application.java

--- src/main/java/ndrdgtl/qa_guru_18/QaGuru18Application.java
+++ src/main/java/ndrdgtl/qa_guru_18/QaGuru18Application.java
@@ -5,8 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class QaGuru18Application {
+    
+    private static String msg = "Hello qa_guru!";
 
     public static void main(String[] args) {
-        System.out.println("Hello qa_guru");
+        System.out.println(msg);
     }
 }
