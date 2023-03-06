50d175ed07b6e2ffa2cb09d48cffe44e38054585
Adel Kurysheva <Adel.Kurysheva@nedra.digital>
2023-03-07 00:28:49 +0300

    init commit

---
+++ src/main/java/ndrdgtl/qa_guru_18/QaGuru18Application.java
@@ -0,0 +1,12 @@
+package ndrdgtl.qa_guru_18;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QaGuru18Application {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ src/test/java/ndrdgtl/qa_guru_18/QaGuru18ApplicationTests.java
@@ -0,0 +1,13 @@
+package ndrdgtl.qa_guru_18;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QaGuru18ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
