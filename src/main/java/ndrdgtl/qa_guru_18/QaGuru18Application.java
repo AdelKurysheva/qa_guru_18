b769e6a366249a250333212d0e7049449a1edc5e
Adel Kurysheva <Adel.Kurysheva@nedra.digital>
2023-03-07 00:56:00 +0300

    added new message

--- src/main/java/ndrdgtl/qa_guru_18/QaGuru18Application.java
+++ src/main/java/ndrdgtl/qa_guru_18/QaGuru18Application.java
@@ -7,6 +7,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class QaGuru18Application {
 
     public static void main(String[] args) {
-        System.out.println("Hello qa_guru");
+        System.out.println(getMessage());
     }
+    private static String getMessage(){
+        return "Hello qa.guru!";
+    }
+
 }
